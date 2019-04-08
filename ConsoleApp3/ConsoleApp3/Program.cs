fa4a669a478d92cf80587d32a7bdca979ad991a4
farbodkain <farbodkain@gmail.com>
2019-04-08 15:39:45 +0430

    ok

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using IronPdf;
+using System.Net;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using System.Web;
+
+//using PdfSharp;
+//using PdfSharp.Drawing;
+//using PdfSharp.Pdf;
+//using PdfSharp.Pdf.IO;
+
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html;
+using iText.Html2pdf;
+using HtmlToPdf;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp3
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      var exePath = Path.GetDirectoryName(System.Reflection
+                   .Assembly.GetExecutingAssembly().CodeBase);
+      Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+      var root= appPathMatcher.Match(exePath).Value;
+      var output = root + "\\"+ "OutPut\\";
+      var templatePath = root + "\\" + "Template.html";
+      string html_content = File.ReadAllText(templatePath);
+
+      var name = Convertor.Html(html_content, output);
+    }
+
+
+
+
+  }
+}
---
+++ ConsoleApp3/HtmlToPdf/Convert.cs
@@ -0,0 +1,25 @@
+﻿using iText.Html2pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlToPdf
+{
+  public static class Convertor
+  {
+    public static string Html(string stringHtml, string outPut)
+    {
+      if(!string.IsNullOrEmpty(outPut))
+      {
+        var name = Guid.NewGuid() + "_To_stream.pdf";
+        FileStream to_stream = new FileStream(outPut + name, FileMode.CreateNew);
+        HtmlConverter.ConvertToPdf(stringHtml, to_stream);
+        return name;
+      }
+      return null;
+    }
+  }
+}
